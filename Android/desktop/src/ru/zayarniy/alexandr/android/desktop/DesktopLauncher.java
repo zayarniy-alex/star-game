805fb9f8bc5c048569e19a328af8ba75bc2dd492
Alexandr Zayarniy <zayarniy.alex@gmail.com>
2018-08-16 22:52:50 +0300

    release v1

---
+++ Android/desktop/src/ru/zayarniy/alexandr/android/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.zayarniy.alexandr.android.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.zayarniy.alexandr.android.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
